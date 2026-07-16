ef9bdaf95f33ce54336d0ecea468b957eb4cb20a
ahmedraza67 <ahmedraza@codiea.io>
2026-07-16 11:55:19 +0500

    lec 5 loop

---
+++ js/lec4/loops.js
@@ -0,0 +1,42 @@
+// for loop
+// forOf loop
+
+// while loop
+// do while loop
+
+// for (starting point ; condition ; increment){
+//     code
+// }
+
+let array = ["ahmed", "sdasd", "asdwwww", "fffff"];
+console.log();
+
+let table = 2;
+// 2 * 1 = 2
+for (let start = 0; start < array.length; start++) {
+  console.log("hello", array[start]);
+}
+
+// *
+// * *
+// * * *
+// * * * *
+
+// *
+// * *
+// * * *
+// * * * *
+// * * *
+// * *
+// *
+for (const hello of array) {
+  console.log("hello", hello);
+}
+
+// while (array.length < 4) {
+//   console.log("ello");
+// }
+
+// do {
+//   console.log("ello");
+// } while (array.length < 5);
