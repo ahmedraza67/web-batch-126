bc9461a8c7d299451b8bbf5b68d378aa7a6d3278
ahmedraza67 <ahmedraza@codiea.io>
2026-07-21 12:12:18 +0500

    functions lec 7

---
+++ js/lec7/arrayMethods.js
@@ -0,0 +1,447 @@
+const stdData = [
+  {
+    id: 1,
+    fName: "Ahmed",
+    lName: "Raza",
+    rollNo: 20,
+    class: "9th",
+    marks: 479,
+  },
+  { id: 2, fName: "Ali", lName: "Khan", rollNo: 21, class: "9th", marks: 450 },
+  {
+    id: 3,
+    fName: "Usman",
+    lName: "Ahmed",
+    rollNo: 22,
+    class: "9th",
+    marks: 468,
+  },
+  {
+    id: 4,
+    fName: "Hassan",
+    lName: "Iqbal",
+    rollNo: 23,
+    class: "9th",
+    marks: 485,
+  },
+  {
+    id: 5,
+    fName: "Bilal",
+    lName: "Sheikh",
+    rollNo: 24,
+    class: "9th",
+    marks: 440,
+  },
+  {
+    id: 6,
+    fName: "Hamza",
+    lName: "Malik",
+    rollNo: 25,
+    class: "9th",
+    marks: 472,
+  },
+  { id: 7, fName: "Saad", lName: "Rauf", rollNo: 26, class: "9th", marks: 458 },
+  {
+    id: 8,
+    fName: "Awais",
+    lName: "Butt",
+    rollNo: 27,
+    class: "9th",
+    marks: 495,
+  },
+  {
+    id: 9,
+    fName: "Shahzaib",
+    lName: "Nawaz",
+    rollNo: 28,
+    class: "9th",
+    marks: 435,
+  },
+  {
+    id: 10,
+    fName: "Danish",
+    lName: "Aslam",
+    rollNo: 29,
+    class: "9th",
+    marks: 462,
+  },
+  {
+    id: 11,
+    fName: "Farhan",
+    lName: "Qureshi",
+    rollNo: 30,
+    class: "9th",
+    marks: 489,
+  },
+  {
+    id: 12,
+    fName: "Adnan",
+    lName: "Saeed",
+    rollNo: 31,
+    class: "9th",
+    marks: 471,
+  },
+  {
+    id: 13,
+    fName: "Tahir",
+    lName: "Yousaf",
+    rollNo: 32,
+    class: "9th",
+    marks: 452,
+  },
+  {
+    id: 14,
+    fName: "Zain",
+    lName: "Haider",
+    rollNo: 33,
+    class: "9th",
+    marks: 477,
+  },
+  {
+    id: 15,
+    fName: "Huzaifa",
+    lName: "Khalid",
+    rollNo: 34,
+    class: "9th",
+    marks: 466,
+  },
+  {
+    id: 16,
+    fName: "Abdullah",
+    lName: "Noor",
+    rollNo: 35,
+    class: "9th",
+    marks: 498,
+  },
+  {
+    id: 17,
+    fName: "Sami",
+    lName: "Javed",
+    rollNo: 36,
+    class: "9th",
+    marks: 443,
+  },
+  {
+    id: 18,
+    fName: "Talha",
+    lName: "Mirza",
+    rollNo: 37,
+    class: "9th",
+    marks: 480,
+  },
+  {
+    id: 19,
+    fName: "Mubashir",
+    lName: "Iqbal",
+    rollNo: 38,
+    class: "9th",
+    marks: 451,
+  },
+  {
+    id: 20,
+    fName: "Imran",
+    lName: "Saleem",
+    rollNo: 39,
+    class: "9th",
+    marks: 473,
+  },
+  {
+    id: 21,
+    fName: "Kamran",
+    lName: "Hussain",
+    rollNo: 40,
+    class: "9th",
+    marks: 469,
+  },
+  {
+    id: 22,
+    fName: "Asad",
+    lName: "Akram",
+    rollNo: 41,
+    class: "9th",
+    marks: 454,
+  },
+  {
+    id: 23,
+    fName: "Fahad",
+    lName: "Jamil",
+    rollNo: 42,
+    class: "9th",
+    marks: 487,
+  },
+  {
+    id: 24,
+    fName: "Waqas",
+    lName: "Latif",
+    rollNo: 43,
+    class: "9th",
+    marks: 445,
+  },
+  {
+    id: 25,
+    fName: "Noman",
+    lName: "Shah",
+    rollNo: 44,
+    class: "9th",
+    marks: 474,
+  },
+  {
+    id: 26,
+    fName: "Sameer",
+    lName: "Aziz",
+    rollNo: 45,
+    class: "9th",
+    marks: 491,
+  },
+  {
+    id: 27,
+    fName: "Rizwan",
+    lName: "Majeed",
+    rollNo: 46,
+    class: "9th",
+    marks: 438,
+  },
+  {
+    id: 28,
+    fName: "Owais",
+    lName: "Farooq",
+    rollNo: 47,
+    class: "9th",
+    marks: 460,
+  },
+  {
+    id: 29,
+    fName: "Adeel",
+    lName: "Rehman",
+    rollNo: 48,
+    class: "9th",
+    marks: 482,
+  },
+  {
+    id: 30,
+    fName: "Shayan",
+    lName: "Nadeem",
+    rollNo: 49,
+    class: "9th",
+    marks: 455,
+  },
+  {
+    id: 31,
+    fName: "Yasir",
+    lName: "Kaleem",
+    rollNo: 50,
+    class: "9th",
+    marks: 493,
+  },
+  {
+    id: 32,
+    fName: "Arslan",
+    lName: "Butt",
+    rollNo: 51,
+    class: "9th",
+    marks: 467,
+  },
+  {
+    id: 33,
+    fName: "Haris",
+    lName: "Rana",
+    rollNo: 52,
+    class: "9th",
+    marks: 459,
+  },
+  {
+    id: 34,
+    fName: "Junaid",
+    lName: "Ashraf",
+    rollNo: 53,
+    class: "9th",
+    marks: 481,
+  },
+  {
+    id: 35,
+    fName: "Shoaib",
+    lName: "Qadir",
+    rollNo: 54,
+    class: "9th",
+    marks: 446,
+  },
+  {
+    id: 36,
+    fName: "Rehan",
+    lName: "Khan",
+    rollNo: 55,
+    class: "9th",
+    marks: 470,
+  },
+  {
+    id: 37,
+    fName: "Umair",
+    lName: "Rasheed",
+    rollNo: 56,
+    class: "9th",
+    marks: 488,
+  },
+  {
+    id: 38,
+    fName: "Nabeel",
+    lName: "Siddiqui",
+    rollNo: 57,
+    class: "9th",
+    marks: 444,
+  },
+  {
+    id: 39,
+    fName: "Ibrahim",
+    lName: "Tariq",
+    rollNo: 58,
+    class: "9th",
+    marks: 476,
+  },
+  {
+    id: 40,
+    fName: "Anas",
+    lName: "Shafiq",
+    rollNo: 59,
+    class: "9th",
+    marks: 453,
+  },
+  {
+    id: 41,
+    fName: "Faizan",
+    lName: "Riaz",
+    rollNo: 60,
+    class: "9th",
+    marks: 490,
+  },
+  {
+    id: 42,
+    fName: "Muneeb",
+    lName: "Zafar",
+    rollNo: 61,
+    class: "9th",
+    marks: 457,
+  },
+  {
+    id: 43,
+    fName: "Sufyan",
+    lName: "Amin",
+    rollNo: 62,
+    class: "9th",
+    marks: 478,
+  },
+  {
+    id: 44,
+    fName: "Zeeshan",
+    lName: "Bashir",
+    rollNo: 63,
+    class: "9th",
+    marks: 449,
+  },
+  {
+    id: 45,
+    fName: "Mohsin",
+    lName: "Anwar",
+    rollNo: 64,
+    class: "9th",
+    marks: 484,
+  },
+  {
+    id: 46,
+    fName: "Rameez",
+    lName: "Iqbal",
+    rollNo: 65,
+    class: "9th",
+    marks: 461,
+  },
+  {
+    id: 47,
+    fName: "Ahsan",
+    lName: "Mahmood",
+    rollNo: 66,
+    class: "9th",
+    marks: 496,
+  },
+  {
+    id: 48,
+    fName: "Shahmeer",
+    lName: "Zubair",
+    rollNo: 67,
+    class: "9th",
+    marks: 442,
+  },
+  {
+    id: 49,
+    fName: "Kashif",
+    lName: "Hanif",
+    rollNo: 68,
+    class: "9th",
+    marks: 475,
+  },
+  {
+    id: 50,
+    fName: "Ehsan",
+    lName: "Rauf",
+    rollNo: 69,
+    class: "9th",
+    marks: 456,
+  },
+];
+
+// for (let index = 0; index < stdData.length; index++) {
+//   if (stdData[index].marks >= 490) {
+//     console.log(stdData[index]);
+//   }
+// }
+
+// if (stdData[0].marks >= 49) {
+//   return stdData[0].marks;
+// }
+
+// map
+// stdData.map()
+
+// filter
+
+resultCalc();
+
+// reduce
+
+function resultCalc(name, marks, total) {
+  let percent = (marks / 500) * 100;
+
+  switch (true) {
+    case percent < 40 && percent > 33:
+      console.log("std pass");
+
+      break;
+    case percent > 40 && percent < 70:
+      console.log("std passed with b grade");
+
+      break;
+    case percent > 70:
+      console.log("std passed with a grade");
+
+      break;
+
+    default:
+      console.log("std Failed");
+
+      break;
+  }
+
+  console.log("welldone");
+  console.log(percent);
+  
+  return percent;
+}
+
+let percentasddd = resultCalc("ahmed", 398, 500);
+console.log(percentasddd);
+
+resultCalc("raza", 120, 500);
+resultCalc("raza", 280, 500);
+
+
+// console.log();
+
+// heading = percent
