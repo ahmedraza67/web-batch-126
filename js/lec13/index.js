896b4afd16b07892af25aa28c6b3de7f3f5b91a7
ahmedraza67 <ahmedraza@codiea.io>
2026-07-30 11:23:19 +0500

    dom

---
+++ js/lec13/index.js
@@ -0,0 +1,16 @@
+// let heading = document.getElementsByTagName;
+// let heading = document.getElementsByClassName;
+// let heading = document.getElementById;
+// let heading = document.querySelector;
+// let heading = document.querySelectorAll;
+
+// let mainHeading = document.getElementsByTagName("h1");
+// let mainHeading = document.getElementsByClassName("mainHeading");
+// let mainHeading = document.getElementById("mainhead");
+let mainHeading = document.querySelector("#mainhead");
+// let mainHeading = document.querySelectorAll(".mainHeading");
+console.log(mainHeading);
+
+// mainHeading.textContent = "<strong>ahmed</strong>";
+// mainHeading.innerHTML = "<strong>ahmed</strong>";
+mainHeading.style.color = "red";
